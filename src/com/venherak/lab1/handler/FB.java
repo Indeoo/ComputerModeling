35eb8f770a9d7cdc5887c76b9acdee422ab93f0a
Arseniy Venherak <indeooars@gmail.com>
2017-11-15 00:44:34 +0200

    Commit 1

---
+++ src/com/venherak/lab1/handler/FB.java
@@ -0,0 +1,12 @@
+package com.venherak.lab1.handler;
+
+public class FB extends AbstractHandler {
+    public FB(double miu) {
+        super(miu);
+    }
+
+    @Override
+    public void performCurrentTask() {
+
+    }
+}
